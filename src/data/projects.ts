ae7ad7b13a6a50cd77da72c0990f317d4aaf8a7d
shivaprasad <shivaprasadrms@gmail.com>
2026-05-24 08:13:34 +0530

    Add projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -38,5 +38,41 @@ export const projects: Project[] = [
       },
     ],
   },
+    {
+    name: "Boardspace - Collaborative Whiteboard",
+    summary: "Real-time collaborative whiteboard built with ASP.NET Core MVC, SignalR, and HTML5 Canvas. Supports multi-user drawing, live synchronization, and persistent storage of drawing elements.",
+    techStack: "C#, ASP.NET Core, EF Core, SignalR",
+    image: "/projects/localshare-android.webp",
+    links: [
+      {
+        label: "GitHub",
+        url: "https://github.com/shivaprasadms/BoardSpace---Collaborative-Whiteboard",
+      },
+    ],
+  },
+    {
+    name: "Voyage AI - Multi Agent Travel Planner",
+    summary: "AI-powered travel planning application with multiple autonomous agents for itinerary creation and optimization.",
+    techStack: "Python, Langgraph, streamlit",
+    image: "/projects/voyage.webp",
+    links: [
+      {
+        label: "GitHub",
+        url: "https://github.com/shivaprasadms/Voyage-AI---Multi-Agent-Travel-Planner",
+      },
+    ],
+  },
+    {
+    name: "EDURAG - Syllabus-Aware Examination & Grading System",
+    summary: "Syllabus-aware RAG platform for automated exam generation & multimodal grading.",
+    techStack: "python,langchain, streamlit",
+    image: "/projects/localshare-android.webp",
+    links: [
+      {
+        label: "GitHub",
+        url: "https://github.com/shivaprasadms/EduRag",
+      },
+    ],
+  },
 
 ];
